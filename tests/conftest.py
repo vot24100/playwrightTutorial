2a8610327891ea3c23975fbeb5cb2ba8592c1235
vot24100 <mladen.m.savov@gmail.com>
2023-10-16 12:19:33 +0300

    Using page fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,9 +10,13 @@ from playwright.sync_api import Playwright
 #
 #     yield page
 
-
-@pytest.fixture(scope='session')
-def set_up(browser):
-    context = browser.new_context()
-    page = context.new_page()
+@pytest.fixture()
+def set_up(page):
     yield page
+
+
+# @pytest.fixture(scope='session')
+# def set_up(browser):
+#     context = browser.new_context()
+#     page = context.new_page()
+#     yield page
